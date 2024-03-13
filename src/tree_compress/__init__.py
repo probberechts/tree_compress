2791f1d48e0428ab9eccd4bf0059f7987fe7a4a8
Laurens Devos <laurens.devos@outlook.com>
2024-03-13 19:27:25 +0100

    Example notebook

--- src/tree_compress/__init__.py
+++ src/tree_compress/__init__.py
@@ -1,1 +1,4 @@
+# ruff: noqa: F401
+
 from .compress import Data, Compress, CompressRecord
+from .aux import compress_topdown, refine_leafs, prune_ensemble
---
+++ src/tree_compress/aux.py
@@ -0,0 +1,25 @@
+import veritas
+from .compress import Compress
+
+def compress_topdown(data, clf, relerr=0.01, max_rounds=2, return_compress_object=False, silent=False):
+    if isinstance(clf, veritas.AddTree):
+        at = clf
+    else:
+        at = veritas.get_addtree(clf)
+
+    compress = Compress(data, at, silent=silent)
+    at_pruned = compress.compress(relerr, max_rounds)
+
+    if return_compress_object:
+        return compress
+    else:
+        return at_pruned
+
+
+def refine_leafs(data, clf):
+    pass
+
+
+def prune_ensemble(data, clf):
+    pass
+
